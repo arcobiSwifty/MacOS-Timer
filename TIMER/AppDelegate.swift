49356183f9a20aa82e2b751a859ccca54fd09bfb
arcobiSwifty <33202233+arcobiSwifty@users.noreply.github.com>
2018-10-12 11:32:06 +0200

    Initial Commit

---
+++ TIMER/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TIMER
+//
+//  Created by Vincent Bossaerts on 12/10/2018.
+//  Copyright © 2018 Vincent Bossaerts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
